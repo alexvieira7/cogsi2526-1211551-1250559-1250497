4ace9812b6bc79066b179ad87f6eb6cc9824a1d8
sofissg13 <1250559@isep.ipp.pt>
2025-11-26 18:57:18 +0000

    correção

---
+++ CA6/CA6-part1/spring-example/src/integrationTest/java/com/exemplo/IntegrationTest.java
@@ -0,0 +1,13 @@
+package com.exemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegrationTest {
+    @Test
+    void teste(){
+        System.out.println("A executar testes");
+
+    }
+
+}
