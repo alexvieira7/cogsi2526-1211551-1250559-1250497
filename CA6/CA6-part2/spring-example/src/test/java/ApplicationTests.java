7732c931032b6b1fb9c7593149d6ec7b9d39bf47
sofissg13 <1250559@isep.ipp.pt>
2025-12-01 17:35:55 +0000

    CA6-part2: ficheiro de testes

---
+++ CA6/CA6-part2/spring-example/src/test/java/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.exemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationTests {
+
+    @Test
+    void sampleUnitTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
